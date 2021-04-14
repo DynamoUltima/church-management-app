78588268b55f32388e4b32fedc47032af8cbe20c
DynamoUltima <dynamo.joey@gmail.com>
2021-04-14 13:26:39 +0000

    first commit

---
+++ src/good.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+
+export interface hiProps {
+    
+}
+ 
+const hi: React.SFC<hiProps> = () => {
+    return ( <div></div> );
+}
+ 
+export default hi;
